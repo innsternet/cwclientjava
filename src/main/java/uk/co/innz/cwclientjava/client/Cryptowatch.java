26a991ff3f9bbfb3fbef00060eec0c76cb932737
Ryan <admin@innz.co.uk>
2019-07-12 08:49:02 +0100

    Initial Commit

---
+++ src/main/java/uk/co/innz/cwclientjava/client/Cryptowatch.java
@@ -0,0 +1,15 @@
+package uk.co.innz.cwclientjava.client;
+
+public class Cryptowatch {
+
+	private String API_KEY = "";
+	private String SECRET_KEY = "";
+	
+	public Cryptowatch(String apiKey, String secretKey) {
+		API_KEY = apiKey;
+		SECRET_KEY = secretKey;
+	}
+	
+	
+	
+}
